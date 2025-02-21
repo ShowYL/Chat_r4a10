a691a27fe2bf5b4979da2f4d0c54ed645bee8256
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2025-02-21 15:28:32 +0100

    Refactor message handling to store input values in variables; update sending logic

--- script/script.js
+++ script/script.js
@@ -4,9 +4,17 @@ import getXHR from './utils.js';
 const pseudo = document.getElementById('pseudo');
 const message = document.getElementById('message');
 const button = document.getElementById('button-send');
-pseudo.addEventListener('keydown', handleKeyDown);
-message.addEventListener('keydown', handleKeyDown);
+let pseudoValue = "";
+let messageValue = "";
+pseudo.addEventListener('input', (event) => {
+    (pseudoValue = event.target.value);
+});
+message.addEventListener('input', (event) => {
+    (messageValue = event.target.value);
+});
 button.addEventListener('click', envoyer);
+
+
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -29,11 +37,11 @@ function fetchMessages(){
 }
 
 async function envoyer(){
-    let toSend = {"pseudo":pseudo,"message":message}
+    let toSend = {"pseudo":pseudoValue,"message":messageValue}
     getXHR('http://localhost:3001/api/envoyerMessage/endpoint.php',"POST",JSON.stringify(toSend))
     .then(data => JSON.parse(data))
     .then(data => console.log(data))
-    .then(() => message="")
+    .then(() => message.value = "")
     .catch(err => console.error(err))
 }
 
