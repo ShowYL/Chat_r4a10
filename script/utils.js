0bf4c79a3e045e1ed0266576163b2434a532504e
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-03-18 10:25:05 +0100

    comment

--- script/utils.js
+++ script/utils.js
@@ -1,16 +1,21 @@
-
 export default async function getXHR(url, method="GET", data=null) {
+    // Créer une promesse et permet donc l' utilisation de methode comme then et catch
     return new Promise((resolve, reject) => {
+        // Créer un nouvel objet XMLHttpRequest
         let xhr = new XMLHttpRequest();
 
+        // Initialiser la requête
         xhr.open(method, url);
 
+        // Définir l'en-tête Content-Type pour les requêtes POST avec des données
         if (data && method === "POST") {
             xhr.setRequestHeader("Content-Type", "application/json");
         }
         
+        // Envoyer la requête avec ou sans données
         data ? xhr.send(data) : xhr.send()
 
+        // Gérer la réponse réussie
         xhr.onload = () => {
             if (xhr.status !== 200) {
                 console.log(`Error ${xhr.status}: ${xhr.statusText}`);
@@ -21,6 +26,7 @@ export default async function getXHR(url, method="GET", data=null) {
             }
         };
 
+        // Gérer les événements de progression du téléchargement
         xhr.onprogress = (event) => {
             if (event.lengthComputable) {
                 console.log(`Received ${event.loaded} of ${event.total} bytes`);
@@ -29,6 +35,7 @@ export default async function getXHR(url, method="GET", data=null) {
             }
         };
 
+        // Gérer les erreurs de requête
         xhr.onerror = () => {
             console.error(`Request failed`);
             reject(`Request failed`);
